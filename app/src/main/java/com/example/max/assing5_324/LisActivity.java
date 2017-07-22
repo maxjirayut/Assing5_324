5ae36dc534ba01621e1d1d08bd8f81b0f51b2421
max <jirayut_max@hotmail.com>
2017-07-22 15:17:51 +0700

    setText showname

--- app/src/main/java/com/example/max/assing5_324/LisActivity.java
+++ app/src/main/java/com/example/max/assing5_324/LisActivity.java
@@ -2,12 +2,21 @@ package com.example.max.assing5_324;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LisActivity extends AppCompatActivity {
+
+    TextView tvShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lis);
+
+        tvShow = (TextView) findViewById(R.id.tvShow);
+        String name = getIntent().getExtras().getString("KEY");
+        tvShow.setText(name);
+
+
     }
 }
--- app/src/main/java/com/example/max/assing5_324/MainActivity.java
+++ app/src/main/java/com/example/max/assing5_324/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.max.assing5_324;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtName;
+    Button btdOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtName = (EditText) findViewById(R.id.edtName);
+        btdOK = (Button) findViewById(R.id.btdOk);
+
+        btdOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edtName.getText().toString();
+                Toast.makeText(getApplicationContext(),"",Toast.LENGTH_LONG).show();
+
+                Intent lis = new Intent(getApplicationContext(), LisActivity.class);
+                lis.putExtra("KEY", name);
+
+                startActivity(lis);
+
+            }
+        });
+
     }
 }
